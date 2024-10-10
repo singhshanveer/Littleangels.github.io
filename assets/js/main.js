7d81297a473e47cd14fdef9dbec6e716fce2046a
SHANVEER SINGH <singhshanveer1@gmail.com>
2024-10-10 18:00:25 -0400

    New Updates
    New Section added and Public Disclosure is also updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,3 +210,31 @@
   });
 
 })(jQuery);
+
+
+
+/**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Init swiper sliders
+   */
+  function initSwiper() {
+    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector(".swiper-config").innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains("swiper-tab")) {
+        initSwiperWithCustomPagination(swiperElement, config);
+      } else {
+        new Swiper(swiperElement, config);
+      }
+    });
+  }
+
+  window.addEventListener("load", initSwiper);
